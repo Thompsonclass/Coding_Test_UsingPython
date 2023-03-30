61675add9fc3f28ded5a5bd22bfaa647efde7372
Thompsonclass <bbb7219@naver.com>
2023-03-30 12:45:47 +0900

    짝수와 홀수 개수

--- Level0-Python/CodingTestExample07.py
+++ Level0-Python/CodingTestExample07.py
@@ -1,6 +1,7 @@
-#정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
-#제한사항 : 1 ≤ num_list의 길이 ≤ 100, 0 ≤ num_list의 원소 ≤ 1,000
-#호출 결과 : num_list = [1, 2, 3, 4, 5], sum_result = [2, 3]
+# 문제 설명
+# 정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+# 제한사항 : 1 ≤ num_list의 길이 ≤ 100, 0 ≤ num_list의 원소 ≤ 1,000
+# 호출 결과 : num_list = [1, 2, 3, 4, 5], sum_result = [2, 3]
 
 def solution(num_list):
     len1 = len(num_list)
@@ -28,8 +29,8 @@ def solution(num_list) :
         answer[i%2] = answer[i%2] + 1
     return answer
     
-#설명 : answer배열의 인덱스를 응용하여 문제 해석
+# 설명 : answer배열의 인덱스를 응용하여 문제 해석
 
-#출력
+# 출력
 print("출력 : " + solution([1, 2, 3, 4, 5]))
 print("출력 : " + solution([1, 2, 3, 4, 5, 7, 8, 8]))
