b07aa4378fdba3937a717f0e56e1aa2555200679
Thompsonclass <bbb7219@naver.com>
2023-03-30 12:50:01 +0900

    특정 문자 제거

--- Level0-Python/CodingTestExample09.py
+++ Level0-Python/CodingTestExample09.py
@@ -1,5 +1,6 @@
-#문제 설명 : 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
-#출력 예시 : my_string = "BCBdbe", letter = "B", result = "Cdbe" 
+# 문제 설명 
+# 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+# 출력 예시 : my_string = "BCBdbe", letter = "B", result = "Cdbe" 
 
 def solution(my_string, letter):
     list_res = []
@@ -8,12 +9,12 @@ def solution(my_string, letter):
             list_res.append(i)
     return ''.join(list_res)
 
-#개선 사항_1 : "컴프리 헨션 형태"사용
+# 개선 사항_1 : "컴프리 헨션 형태"사용
 def solution(my_string, letter):
     return ''.join([c for c in my_string if c != letter])
 
-#설명 : 요소를 하나 하나 꺼내 조건을 걸고, 리스트 형태를 .join함수를 사용해 변환
+# 설명 : 요소를 하나 하나 꺼내 조건을 걸고, 리스트 형태를 .join함수를 사용해 변환
 
-#출력
+# 출력
 print("출력 : " + solution("CSGdfe", "C"))
 print("출력 : " + solution("BCBdbe", "B"))
