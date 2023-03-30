4fafb90e9ee773d50c93051eaefbb5b96d752083
Thompsonclass <bbb7219@naver.com>
2023-03-30 12:49:03 +0900

    정수 이하의 짝수개수

--- Level0-Python/CodingTestExample08.py
+++ Level0-Python/CodingTestExample08.py
@@ -1,22 +1,23 @@
-#정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
-#제한사항 : 1 ≤ n ≤ 100
-#결과 출력 : n = 10, result = [1, 3, 5, 7, 9]
+# 문제 설명정
+# 정수 n이 매개변수로 주어질 때, n 이하의 짝수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+# 제한사항 : 1 ≤ n ≤ 100
+# 결과 출력 : n = 10, result = [1, 3, 5, 7, 9]
 
 def solution(n):
     answer = []
     if 1<=n<=100:
         for k in range(1, n+1):
-            if k % 2 == 1:
+            if k % 2 == 0:
                 answer.append(k)    
         return answer
     
-#개선사항_1 : "컴프리 헨션 형태"사용
+# 개선사항_1 : "컴프리 헨션 형태"사용
 def solution(n):
     answer = []
-    [answer.append(k) for k in range(1, n+1) if 1<=n<=100 if k % 2 == 1]
+    [answer.append(k) for k in range(1, n+1) if 1<=n<=100 if k % 2 == 0]
     return answer
 
-#출력
+# 출력
 print("출력 : " + solution(7))
 print("출력 : " + solution(8))
 
